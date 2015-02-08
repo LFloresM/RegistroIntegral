74abe88d285efe299b7b69cdb21c9ee712774f93
callegari-projects <callegari.projects@gmail.com>
2015-02-08 15:35:00 -0800

    Menu change. Home design change. Nunito

--- RegistroIntegral/App_Start/BundleConfig.cs
+++ RegistroIntegral/App_Start/BundleConfig.cs
@@ -27,10 +27,11 @@ namespace RegistroIntegral
             bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
                      "~/Scripts/knockout-{version}.js"));
 
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                    "~/Content/bootstrap.css",
+                    "~/Content/google-fonts.css",
+                    "~/Content/site.css"));
+
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
